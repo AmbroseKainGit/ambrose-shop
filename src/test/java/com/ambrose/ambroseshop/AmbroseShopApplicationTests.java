bbbb2d3e5aaa7a770668473aeda6bbd612b2c1d0
CristianCeballosDR <cceballos@docred.com>
2025-01-16 12:50:53 -0500

    First commit

---
+++ src/test/java/com/ambrose/ambroseshop/AmbroseShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambrose.ambroseshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbroseShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
